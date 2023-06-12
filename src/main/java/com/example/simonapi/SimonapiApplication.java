139cb5a7f5f63117005df68a201d33e4630e33c5
simonnava <simonnava2112@gmail.com>
2023-06-11 23:07:47 -0300

    push

---
+++ src/main/java/com/example/simonapi/SimonapiApplication.java
@@ -0,0 +1,13 @@
+package com.example.simonapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimonapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimonapiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/simonapi/SimonapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.simonapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimonapiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
